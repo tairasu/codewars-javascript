81ea2b3399635499fdb7bbf7680e5bedf259e3a8
aomizu <marco.tylus24@gmail.com>
2024-06-06 23:30:46 +0900

    added submission

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,9 @@
+// Even or Odd
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+    return number % 2 == 0 ? 'Even' : 'Odd';
+  }
