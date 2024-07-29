9a6d5a23f7d458bc1ff9045c66ac9e500488f6b3
aomizu <marco.tylus24@gmail.com>
2024-07-29 19:20:47 +0900

    added submission

--- 7kyu/the-highest-profit-wins.js
+++ 7kyu/the-highest-profit-wins.js
@@ -14,6 +14,6 @@
 // https://www.codewars.com/kata/559590633066759614000063/train/javascript
 
 function minMax(arr){
-    const sorted = arr.sort((a,b) => a - b);
+const sorted = arr.sort((a,b) => a - b);
     return [sorted[0],sorted[sorted.length-1]];
   }
